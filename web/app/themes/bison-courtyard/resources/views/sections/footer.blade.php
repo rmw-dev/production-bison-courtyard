7c04ef3d25fc76155c670488ebd8f6b971342fdd
John McAulay <john@thegreenroom.org.nz>
2025-10-04 16:20:50 -0600

    fixes footer animations

--- web/app/themes/bison-courtyard/resources/views/sections/footer.blade.php
+++ web/app/themes/bison-courtyard/resources/views/sections/footer.blade.php
@@ -1,10 +1,10 @@
 <footer class="content-info max-w-[1920px] mx-auto relative mt-8 md:mt-24">
   <div>
     @php(dynamic_sidebar('sidebar-footer'))
-    <img src="{{ Vite::asset('resources/images/mountain-range.svg') }}" class="w-full" alt="{!! $siteName !!}">
+    {!! get_svg('resources/images/mountain-range.svg', 'w-full', ['id' => 'mountain-range']) !!}
     <div class="absolute inset-0 flex items-center justify-center text-theme-brown font-[800] text-theme-brown text-base md:text-2xl lg:text-3xl xl:text-5xl w-2/3 lg:w-3/4 mx-auto spacing-">
       <div class="leading-[1.4] text-center self-start h-1/6 md:h-1/3 lg:h-4/5 flex justify-center flex-col">
-        <div class="hidden md:block">
+        <div id="footer-heading" class="hidden md:block">
           <span class="tracking-wider">BISON COURTYARD ON BEAR STREET</span><br />
             eat. drink. shop. explore. revitalize.
           </div>
