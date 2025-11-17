7de46677947079ca11c44af6b4a9bc882b491c84
John McAulay <john@thegreenroom.org.nz>
2025-11-16 17:48:10 -0700

    changes to contact form destination

--- web/app/themes/bison-courtyard/app/filters.php
+++ web/app/themes/bison-courtyard/app/filters.php
@@ -30,3 +30,42 @@ add_filter('rmw_contact_to', function ($to) {
     // Otherwise fall back to whatever was passed in (admin_email)
     return $to;
 });
+
+dd_filter('rmw_parking_to', function ($to) {
+    // Get the ACF option field (Options Page context = 'option')
+    $custom_email = get_field('contact_email_to', 'option');
+
+    // If it's set and looks like a valid email, use it
+    if (!empty($custom_email) && is_email($custom_email)) {
+        return $custom_email;
+    }
+
+    // Otherwise fall back to whatever was passed in (admin_email)
+    return $to;
+});
+
+dd_filter('rmw_leasing_to', function ($to) {
+    // Get the ACF option field (Options Page context = 'option')
+    $custom_email = get_field('contact_email_to', 'option');
+
+    // If it's set and looks like a valid email, use it
+    if (!empty($custom_email) && is_email($custom_email)) {
+        return $custom_email;
+    }
+
+    // Otherwise fall back to whatever was passed in (admin_email)
+    return $to;
+});
+
+dd_filter('rmw_leasing_res_to', function ($to) {
+    // Get the ACF option field (Options Page context = 'option')
+    $custom_email = get_field('contact_email_to', 'option');
+
+    // If it's set and looks like a valid email, use it
+    if (!empty($custom_email) && is_email($custom_email)) {
+        return $custom_email;
+    }
+
+    // Otherwise fall back to whatever was passed in (admin_email)
+    return $to;
+});
