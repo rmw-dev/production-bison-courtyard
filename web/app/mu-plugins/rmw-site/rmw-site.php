48d059408e5c96239b7be421f16ab9fa530ac005
John McAulay <john@thegreenroom.org.nz>
2025-11-16 17:31:46 -0700

    changes to contact form destination

--- web/app/mu-plugins/rmw-site/rmw-site.php
+++ web/app/mu-plugins/rmw-site/rmw-site.php
@@ -9,3 +9,19 @@ require_once __DIR__ . '/RMWSite.php';
 add_action('plugins_loaded', function () {
     (new \RMW\Site\RMWSite())->boot();
 });
+
+add_action('acf/init', function () {
+    if (! function_exists('acf_add_options_page')) {
+        return;
+    }
+
+    acf_add_options_page([
+        'page_title' => 'Theme Options',
+        'menu_title' => 'Theme Options',
+        'menu_slug'  => 'theme-options',       // must match the location slug
+        'capability' => 'manage_options',
+        'redirect'   => false,
+        'position'   => 60,
+        'icon_url'   => 'dashicons-admin-generic',
+    ]);
+});
